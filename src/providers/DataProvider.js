a8605f9e99599a21731b58f5ab4cb156a6efcacd
Nadeem Mohammad <coolmind182006@gmail.com>
2020-08-08 22:42:40 +0530

    table data

--- src/providers/DataProvider.js
+++ src/providers/DataProvider.js
@@ -20,7 +20,14 @@ export default class DataProvider extends Component {
     }
 
     fetchTableData = () => {
-        
+        //Ideally get the data from Backend API using Axios
+        const columns = [{ heading: 'Name', property: 'name' }, { heading: 'Age', property: 'age' }, { heading: 'Sex', property: 'sex' }, { heading: 'Breed', property: 'breed' },]
+        const rows = [{ name: 'Sabrina', age: '6', sex: 'Female', breed: 'Staffordshire' }, { name: 'Max', age: '2', sex: 'Male', breed: 'Boxer' }]
+        let newState = Object.assign({}, this.state);
+        this.setState({
+            ...newState,
+            tableData: {columns: columns, rows: rows}
+        })
     }
 
     render() {
