d0196daa1ccb84862818cae949b8bbe396adf6b9
Nadeem Mohammad <coolmind182006@gmail.com>
2020-08-09 22:00:48 +0530

    refactored

--- src/components/Modal/ModalMultiWizard.js
+++ src/components/Modal/ModalMultiWizard.js
@@ -34,8 +34,8 @@ export default class ModalMultiWizard extends Component {
   };
 
   // Test current step with ternary
-  // _next and _previous functions will be called on button click
-  _next = () =>  {
+  // next and previous functions will be called on button click
+  next = () =>  {
     let currentStep = this.state.currentStep
     // If the current step is 1 or 2, then add one on "next" button click
     currentStep = currentStep >= 2? 3: currentStep + 1
@@ -44,7 +44,7 @@ export default class ModalMultiWizard extends Component {
     })
   }
     
-  _prev = () =>  {
+  prev = () =>  {
     let currentStep = this.state.currentStep
     // If the current step is 2 or 3, then subtract one on "previous" button click
     currentStep = currentStep <= 1? 1: currentStep - 1
@@ -61,7 +61,7 @@ previousButton = () => {
       return (
         <Button 
           className="btn btn-secondary" 
-          type="button" onClick={this._prev}>
+          type="button" onClick={this.prev}>
         Previous
         </Button>
       )
@@ -77,7 +77,7 @@ previousButton = () => {
         <Button 
           className="btn btn-primary float-right" 
           type="button"
-          onClick={this._next}
+          onClick={this.next}
           disabled={!this.state.step1DataCaptured}
           >
         Next
