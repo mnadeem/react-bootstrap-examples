ed161f05266322deb145bde86fc131952c2492c0
Nadeem Mohammad <coolmind182006@gmail.com>
2020-08-13 15:30:29 +0530

    refactored

--- src/components/PopupButton/PopupButton.js
+++ src/components/PopupButton/PopupButton.js
@@ -2,27 +2,31 @@ import React, { Component } from "react";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import { Button, Modal } from "react-bootstrap";
-import RTable from '../Table/RTable'
+import RTable from "../Table/RTable";
 
 export default class PopupButton extends Component {
-
   constructor() {
     super();
     this.state = {
-      showHide: false,
+      show: false,
     };
   }
 
   handleModalShowHide() {
-    this.setState({ showHide: !this.state.showHide });
+    this.setState({ show: !this.state.show });
   }
 
   handleBlur = (e) => {
     this.props.dataContext.state.fetchTableData();
   };
 
+  onRowSelected = (selectedRow) => {
+    this.setState({ selectedRow: selectedRow });
+    console.log(selectedRow);
+  };
+
   render() {
-    const {columns, rows} = this.props.dataContext.state.tableData;
+    const { columns, rows } = this.props.dataContext.state.tableData;
     return (
       <React.Fragment>
         <InputGroup className="mb-3">
@@ -41,20 +45,19 @@ export default class PopupButton extends Component {
             </Button>
           </InputGroup.Append>
         </InputGroup>
-        <Modal show={this.state.showHide}>
+        <Modal show={this.state.show}>
           <Modal.Header closeButton onClick={() => this.handleModalShowHide()}>
             <Modal.Title>Table Modal</Modal.Title>
           </Modal.Header>
 
           <Modal.Body>
-          <RTable
-                columns={columns}
-                rows={rows}
-                propertyAsKey='name'
-                rowSelected = {this.onRowSelected}
-                radioGroupKey = 'tableGroup'
-                />
-
+            <RTable
+              columns={columns}
+              rows={rows}
+              propertyAsKey="name"
+              rowSelected={this.onRowSelected}
+              radioGroupKey="tableGroup"
+            />
           </Modal.Body>
 
           <Modal.Footer>
